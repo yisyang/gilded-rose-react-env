9c7e787915d6f8e7b896ed13e08033b6aa5b21c4
Scott Yang <yisyang@gmail.com>
2021-06-25 12:18:14 -0700

    Initial build subtree commit

---
+++ precache-manifest.d5f9a3a3214298b091646ba54b5e490b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e74255a713909c982f7714ba379809b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "854cfdd91d55d2524a1a",
+    "url": "/static/css/2.be04442c.chunk.css"
+  },
+  {
+    "revision": "a2d8c7677d9ba728ca5b",
+    "url": "/static/css/main.4051573a.chunk.css"
+  },
+  {
+    "revision": "854cfdd91d55d2524a1a",
+    "url": "/static/js/2.5c6771bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5c6771bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d8c7677d9ba728ca5b",
+    "url": "/static/js/main.f06caf56.chunk.js"
+  },
+  {
+    "revision": "9f0133d4a0431de01f55",
+    "url": "/static/js/runtime-main.7b0bc16b.js"
+  }
+]);
