8e8f474220dd8591591d625d191332dedf3091fa
Scott Yang <yisyang@gmail.com>
2021-06-25 12:29:23 -0700

    Fix gh-pages deployment.

---
+++ build/precache-manifest.efd6321c0a101b277da84a12fda347f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca6fcb5565543712afc5da61bfc947c",
+    "url": "/gilded-rose-react-env/index.html"
+  },
+  {
+    "revision": "854cfdd91d55d2524a1a",
+    "url": "/gilded-rose-react-env/static/css/2.be04442c.chunk.css"
+  },
+  {
+    "revision": "694fc3390ad03a9bbc70",
+    "url": "/gilded-rose-react-env/static/css/main.4051573a.chunk.css"
+  },
+  {
+    "revision": "854cfdd91d55d2524a1a",
+    "url": "/gilded-rose-react-env/static/js/2.5c6771bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gilded-rose-react-env/static/js/2.5c6771bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694fc3390ad03a9bbc70",
+    "url": "/gilded-rose-react-env/static/js/main.050ec719.chunk.js"
+  },
+  {
+    "revision": "a55398846cbf9527fbdb",
+    "url": "/gilded-rose-react-env/static/js/runtime-main.139d0cd6.js"
+  }
+]);
